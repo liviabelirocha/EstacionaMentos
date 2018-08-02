ba0d6ba58521638e713fd338fc9738f683a44ccd
GustavoM7 <gustavosmarquesf@gmail.com>
2018-08-02 15:35:43 -0300

    Concluindo função de login

--- js/login.js
+++ js/login.js
@@ -1,14 +1,43 @@
 var submitButton = document.getElementById('submitButton');
+var sendButton = document.getElementById('sendButton');
+//Variáveis para os botões de envar cadastro e enviar login de entrada
 
 //Função para os botões 'entrar' e 'cadastrar' da área de login
 function Revelar(div) {
     var display = document.getElementById(div).style.display;
-    if(display == "none")
+    if (display == "none")
         document.getElementById(div).style.display = 'block';
     else
         document.getElementById(div).style.display = 'none';
 }
 
+//Formulário para login de entrada
+sendButton.addEventListener('click', function () {
+    //Variáveis:
+    var emailInputE = document.getElementById('emailInputEnter');
+    var passInputE = document.getElementById('passInputEnter');
+
+    //Fazendo login:
+    firebase.auth().signInWithEmailAndPassword(emailInputE.value, passInputE.value).then(function () {
+        location.href="user.html";
+    }).catch(function (error) {
+        var errorCode = error.code;
+        //Caso em que email não possui cadastro
+        if (errorCode === 'auth/user-not-found') {
+            document.getElementById('invalidEmailEnter').style.display = 'block';
+        }
+        //Caso em que a senha é inválida
+        else if (errorCode === 'auth/wrong-password') {
+            document.getElementById('invalidPassEnter').style.display = 'block';
+        }
+        //Casos inesperados
+        else{
+            alert("Erro inesperado!");
+        }
+    })
+})
+
+//Formulário de cadastro
 submitButton.addEventListener('click', function () {
     //Variáveis:
     var nameInput = document.getElementById('nameInput');
@@ -16,7 +45,7 @@ submitButton.addEventListener('click', function () {
     var passwordInput = document.getElementById('passwordInput');
     var emailTest = document.getElementById('emailTest');
     var passTest = document.getElementById('passTest');
-    
+
     //verificando se formulário de cadastro é válido
     if (nameInput.value == "")
         document.getElementById('nullName').style.display = 'block';
@@ -31,13 +60,17 @@ submitButton.addEventListener('click', function () {
     else if (passwordInput.value != passTest.value)
         document.getElementById('invalidPass').style.display = 'block';
     //Criando conta:
-    else{
-        firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value).then( function(){
+    else {
+        firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value).then(function () {
             alert("Conta registrada com sucesso!");
+            var user = firebase.auth().currentUser;
+            user.updateProfile({
+                displayName: nameInput.value
+            });
             location.reload();
-        }).catch(function() {
+        }).catch(function () {
             alert("Erro! Este email já possui cadastro!");
-            location.reload(); 
-          });
+            location.reload();
+        })
     }
 });
