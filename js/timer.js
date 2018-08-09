c395299f79063ab1daa9b336b6bba423044c2c18
Lívia <liviabelirocha@outlook.com>
2018-08-08 22:05:29 -0300

    time (1 bug)

--- js/timer.js
+++ js/timer.js
@@ -1,39 +1,52 @@
-var car = document.getElementById('car');
-var moto = document.getElementById('moto');
 var database = firebase.database(); //Referência para banco de dados
-
-var inicio;
+var inicio; 
+var final; 
+var data = new Date();
 
 //Pegar o horario
 function time(){
-	var data = new Date();
-	var horas = data.getHours();
-	var minutos = data.getMinutes();
-	var inicioBruto = horas*60 + minutos;
+	var horas = data.getHours(); 
+	var minutos = data.getMinutes(); 
+	var inicioBruto = horas*60 + minutos; 
+	inicio = horas + ":" + minutos; 
 	
-	inicio = horas + ":" + minutos;
-	
+	//Mandar o horario inicial para o banco de dados
 	firebase.database().ref('users/FPOMh7s4XhXdmlG786DxvbXXCwz2').update({
-		reserva: inicio
+		reservaInicial: inicio,
+		reservaMinutos: inicioBruto
 	})
 }
 
-var data = new Date();
-var horasf = data.getHours();
-var minutosf = data.getMinutes();
-var horariof = horasf + "+" + minutosf;
-var finalBruto = horasf*60 + minutosf;
-
-function atualizarTime(){
-	firebase.database().ref('users/FPOMh7s4XhXdmlG786DxvbXXCwz2/reserva').once('value').then(function(snapshot) {
-		var reserva = snapshot.val();
-		var tempoDecorrido = (finalBruto - reserva)/60;
-		tempoDecorrido = tempoDecorrido.toFixed(2);
-		tempoDecorrido = tempoDecorrido.toString();
-		tempoDecorrido = tempoDecorrido.replace(".", ":");
-
-		firebase.database().ref('users/FPOMh7s4XhXdmlG786DxvbXXCwz2').update({
-			total: tempoDecorrido
-		})
-	});
+//Função para cada vez que a pagina é atualizada
+function timeFinal(){
+	var horasf = data.getHours(); 
+	var minutosf = data.getMinutes(); 
+	var finalBruto = horasf*60 + minutosf; 
+	final = horasf + ":" + minutosf; 
+	
+	function atualizarTime(){
+		//Puxa o tempo inicial em minutos do banco de dados
+		firebase.database().ref('users/FPOMh7s4XhXdmlG786DxvbXXCwz2/reservaMinutos').once('value').then(function(snapshot) {
+			var reservaMinutos = snapshot.val();
+			reservaMinutos = parseFloat(reservaMinutos);
+			var tempoDecorrido = finalBruto - reservaMinutos;
+			//Caso onde já passou uma hora ou mais
+			if (tempoDecorrido >= 60 ){
+				tempoDecorrido = tempoDecorrido/60;
+			}else{ //Caso onde ainda não passou uma hora
+				tempoDecorrido = tempoDecorrido/100;
+			}
+			tempoDecorrido = tempoDecorrido.toFixed(2); //Coloca duas casas decimais depois da virgula
+			tempoDecorrido = tempoDecorrido.toString(); //Transforma o Float em String
+			tempoDecorrido = tempoDecorrido.replace(".", ":"); //Substitui o "." por ":"
+			
+			//Mandar o tempo passado para o banco de dados
+			firebase.database().ref('users/FPOMh7s4XhXdmlG786DxvbXXCwz2').update({
+				reservaFinal: final,
+				total: tempoDecorrido
+			})
+		});
+	}
+	atualizarTime();
 }
+
